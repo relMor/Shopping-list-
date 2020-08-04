db17d1f24ce915d53a599ebbb37d3eef46a371b3
realMortred <amanvarma.600847@gmail.com>
2020-08-04 18:56:57 +0530

    Added Auth and managed state by using useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,61 +1,130 @@
-import React, { useState, useEffect } from "react";
+import React, { useCallback, useReducer, useMemo } from "react";
 import axios from "axios";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
-const Ingredients = (props) => {
-  const [userIngredients, setUserIngredients] = useState([]);
+const ingredientsReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get here!");
+  }
+};
 
-  useEffect(() => {
-    axios
-      .get("https://react-hooks-update-227d7.firebaseio.com/ingredients.json")
-      .then((response) => {
-        const loadedIngredients = [];
-        for (const key in response.data) {
-          loadedIngredients.push({
-            id: key,
-            title: response.data[key].title,
-            amount: response.data[key].amount,
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      })
-      .catch((err) => console.log(err));
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...curHttpState, loading: false };
+    case "ERROR":
+      return { loading: false, error: action.errorData };
+    case "CLEAR":
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error("Should not get here!");
+  }
+};
+
+const Ingredients = (props) => {
+  const [userIngredients, dispatch] = useReducer(ingredientsReducer, []);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
+
+  // const [userIngredients, setUserIngredients] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
-  const addIngredientHandler = (ingredient) => {
+  const addIngredientHandler = useCallback((ingredient) => {
+    // setIsLoading(true);
+    dispatchHttp({ type: "SEND" });
     axios
       .post(
         "https://react-hooks-update-227d7.firebaseio.com/ingredients.json",
         ingredient
       )
       .then((response) => {
-        setUserIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: response.data.name, ...ingredient },
-        ]);
+        // setIsLoading(false);
+        dispatchHttp({ type: "RESPONSE" });
+        //   setUserIngredients((prevIngredients) => [
+        //     ...prevIngredients,
+        //     { id: response.data.name, ...ingredient },
+        //   ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: response.data.name, ...ingredient },
+        });
       })
-      .catch((err) => console.log(err));
-  };
+      .catch((err) => {
+        //setError("Something went wrong!");
+        dispatchHttp({ type: "ERROR", errorData: "Something went wrong!" });
+      });
+  }, []);
 
-  const removeIngredientHandler = (ingredientId) => {
-    setUserIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+  const removeIngredientHandler = useCallback((ingredientId) => {
+    //setIsLoading(true);
+    dispatchHttp({ type: "SEND" });
+    axios
+      .delete(
+        `https://react-hooks-update-227d7.firebaseio.com/ingredients/${ingredientId}.json`
+      )
+      .then((response) => {
+        //setIsLoading(false);
+        dispatchHttp({ type: "RESPONSE" });
+        // setUserIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        // );
+        dispatch({ type: "DELETE", id: ingredientId });
+      })
+      .catch((err) => {
+        //setError("Something went wrong!")
+        dispatchHttp({ type: "ERROR", errorData: "Something went wrong!" });
+      });
+  }, []);
+
+  const clearError = useCallback(() => {
+    //setError(null);
+    dispatchHttp({ type: "CLEAR" });
+  }, []);
+
+  const ingredientsList = useMemo(() => {
+    return (
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
     );
-  };
+  }, [userIngredients, removeIngredientHandler]);
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {httpState.error && (
+        <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>
+      )}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={httpState.loading}
+      />
 
       <section>
-        <Search />
-        <IngredientList
-          ingredients={userIngredients}
-          onRemoveItem={removeIngredientHandler}
-        />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        {ingredientsList}
       </section>
     </div>
   );
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        axios
+          .get(
+            "https://react-hooks-update-227d7.firebaseio.com/ingredients.json" +
+              query
+          )
+          .then((response) => {
+            const loadedIngredients = [];
+            for (const key in response.data) {
+              loadedIngredients.push({
+                id: key,
+                title: response.data[key].title,
+                amount: response.data[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          })
+          .catch((err) => console.log(err));
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
